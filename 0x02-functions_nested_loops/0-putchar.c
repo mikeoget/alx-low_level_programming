c9ad77973bbf297eb66c52e364034bf3d40bca0a
mikeoget <ogetomichael7@gmail.com>
2022-03-15 14:05:25 -0700

    0 commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,21 @@
+#include <unistd.h>
 #include "main.h"
 /**
- * main - prints _putchar
+ * main - Putchar
  *
- * Description: prints _putchar
- *
- * Return: 0 (Success)
+ * _putchar print the characters given
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char text[9] = "_putchar";
-	int i = 0;
-
-	for(i = 0; i<8; i++)
-	{
-		_putchar(text[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-	return(0);
-}	
+	return (0);
+}
