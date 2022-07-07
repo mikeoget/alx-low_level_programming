5ba040230276bd4d9eb08f10a62b197f3d2a1c13
Michael Ogeto <84995011+mikeoget@users.noreply.github.com>
2022-07-07 10:32:14 +0300

    commit 2

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+/**
+ * key_index - finds the index given a key
+ * @key: the key
+ * @size: size of the array
+ *
+ * Return: the index
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
